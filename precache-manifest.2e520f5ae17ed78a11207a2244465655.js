e90acaa5d2e3c25ec7d97c824bad63dd9a424061
Anboias <biasinovschi@gmail.com>
2020-04-25 20:13:41 +0300

    initial commit

---
+++ precache-manifest.2e520f5ae17ed78a11207a2244465655.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78dd18dcd09b229f6181455b51db4ea1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b52f855e0ebc54efc64",
+    "url": "/static/css/main.a7cbc389.chunk.css"
+  },
+  {
+    "revision": "42eb4e71029e6cecfb75",
+    "url": "/static/js/2.db34d693.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.db34d693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b52f855e0ebc54efc64",
+    "url": "/static/js/main.b84ebb19.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
